9ce840933947f8627dabf4f4c53706fcb4609164
Nicolae Ion Sirbu <nicolaeion.sirbu@endava.com>
2021-12-06 10:04:28 +0200

    Add project files.

---
+++ SimpleAPI.Persistance/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleAPI.Domain.Models;
+
+namespace SimpleAPI.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ SimpleAPI.Persistance/ServicesExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleAPI.Persistance
+{
+    public static class ServicesExtension
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
